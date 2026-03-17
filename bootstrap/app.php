7b6598063cda51b4f445a54810ab64959c93642b
Esteban Florez <98498568+esteban-florez@users.noreply.github.com>
2026-03-16 21:31:07 -0400

    Add middleware to trust proxies in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
       'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
     ]);
   })
+  ->withMiddleware(function (Middleware $middleware) {
+    $middleware->trustProxies(at: '*');
+  })
   ->withExceptions(function (Exceptions $exceptions): void {
     //
   })->create();
